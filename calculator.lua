90e6df424d151f5e5a3705fbec3dc1cbdd91b210
Marcellus Chesebro <chesebma@eos11.cis.gvsu.edu>
2017-12-09 13:26:40 -0500

    initial commit

---
+++ calculator.lua
@@ -0,0 +1,30 @@
+total = 0
+
+io.write("total: ")
+io.write(total)
+repeat
+	io.write("\noperation(+, -, *, /) or 'q' to quit: ")
+	operation = io.read()
+	
+	if operation == "+" then	
+		io.write("\namount: ")
+		total = total + io.read()
+	elseif operation == "-" then
+		io.write("\namount: ")
+		total = total - io.read()
+
+	elseif operation == "*" then
+		io.write("\namount: ")
+		total = total * io.read()
+
+	elseif operation == "/" then
+		io.write("\namount: ")
+		total = total / io.read()
+
+	end
+	
+	io.write("\ntotal: ")
+	io.write(total)
+
+
+until operation == "q"
